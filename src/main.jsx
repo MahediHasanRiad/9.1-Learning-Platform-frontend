08ece26de8367f2c9ba6218333a8fd878b019b8b
Mahedi Hasan Riad <contact.mahedihasan@gmail.com>
2026-03-14 23:29:28 +0600

    bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
-
 import App from "./App/App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { Toaster } from "./components/UI/sonner";
 
 
 
